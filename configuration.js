ecb3b4108235dd769ed97b1ff41916d934dcf6a8
Saurabh Mukherjee <44134523+SM1989@users.noreply.github.com>
2018-10-30 00:41:14 +0530

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec1_jasmine_datadriven.js'],
  capabilities: {
 	  'browserName': 'chrome'
